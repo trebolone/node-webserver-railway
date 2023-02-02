247b2b88a3ab891aeb97548495be66f263f25e31
Mario Bros <serfikreik@gmail.com>
2023-02-02 13:15:10 -0600

    segundo cambio

--- index.js
+++ index.js
@@ -39,5 +39,5 @@ app.get( '*', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port at http://localhost:${port}`)
   })
